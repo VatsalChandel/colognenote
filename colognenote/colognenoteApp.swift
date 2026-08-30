21790f497908c984f1893483cf190fa885bb0140
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2026-08-29 21:29:19 -0700

    Initial Commit

---
+++ colognenote/colognenoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  colognenoteApp.swift
+//  colognenote
+//
+//  Created by Vatsal Chandel on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct colognenoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
